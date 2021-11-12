ce25664f0b1e057bcbda6a2df0efe3e79436710f
Nachiket Jadhav <44014957+NAchiket06@users.noreply.github.com>
2021-11-12 23:29:34 +0530

    124. Code refactoring

--- Source/Escape/Grabber.cpp
+++ Source/Escape/Grabber.cpp
@@ -12,18 +12,67 @@ UGrabber::UGrabber()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
-
 
 // Called when the game starts
 void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	UE_LOG(LogTemp, Warning, TEXT("Grabber active."));
+	FindPhysicsHandle();
+	SetupInputComponent();
+	
+}
+
+/*
+	Check if PhysicsHandle Exists
+*/
+void UGrabber::FindPhysicsHandle()
+{
+	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	if (!PhysicsHandle)
+	{
+		UE_LOG(LogTemp, Error, TEXT("No UPhysicsHandleComponent found on %s"), *GetOwner()->GetName());
+	}
+}
+
+/*
+ *  Setup InputComponent and SetBinds for keys
+ */
+void UGrabber::SetupInputComponent()
+{
+	InputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
+
+	if (!InputComponent)
+	{
+		UE_LOG(LogTemp, Error, TEXT("No InputComponent found on %s"), *GetOwner()->GetName());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("InputComponent found on %s"), *GetOwner()->GetName());
+		InputComponent->BindAction("Grab", IE_Pressed, this, &UGrabber::Grab);
+		InputComponent->BindAction("Grab", IE_Released, this, &UGrabber::Release);
+	}
+}
+
+
+/*
+	Get Actors with physics collision and attach the physics handle
+*/
+void UGrabber::Grab()
+{
+	/*
+		UE_LOG(LogTemp, Warning, TEXT("Grabber pressed."));
+	*/
+
+	GetFirstPhysicsBodyInReach();
+}
+
+void UGrabber::Release()
+{
+	/*
+		UE_LOG(LogTemp, Warning, TEXT("Grabber released."));
+	*/
 }
 
 
@@ -31,29 +80,40 @@ void UGrabber::BeginPlay()
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}	
+
+
+FHitResult UGrabber::GetFirstPhysicsBodyInReach() const
+{
 
 	FVector PlayerLocation;
 	FRotator PlayerRotation;
+
 	// Get players viewport
-	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT PlayerLocation,OUT PlayerRotation);
-	
-	//UE_LOG(LogTemp, Warning, TEXT("Player Location: %s, Player Rotation: %s"), *PlayerLocation.ToString(), *PlayerRotation.ToString());
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT PlayerLocation, OUT PlayerRotation);
 
 	FVector LineTraceEnd = PlayerLocation + PlayerRotation.Vector() * Reach;
 
-	DrawDebugLine(
-		GetWorld(), 
+
+	// Store hit results of the raycast
+	FCollisionQueryParams TraceParams(FName(TEXT("")), false, GetOwner());
+
+	FHitResult Hit;
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
 		PlayerLocation,
 		LineTraceEnd,
-		FColor().Red,
-		false,
-		0.f,
-		0,
-		2.f
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParams
 	);
 
-	// Ray Cast to certain distance
+	// Chek raycast 
+	AActor* ActorHit = Hit.GetActor();
 
-	//  Check raycast hit
-}	
+	if (ActorHit)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Hit: %s."), *ActorHit->GetName());
+	}
 
+	return Hit;
+}
--- Source/Escape/Grabber.h
+++ Source/Escape/Grabber.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
+#include "Components/InputComponent.h"
 #include "Grabber.generated.h"
 
 
@@ -16,15 +18,28 @@ public:
 	// Sets default values for this component's properties
 	UGrabber();
 
-protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+
+protected:
+
+	virtual void BeginPlay() override;
 	
 private:
+
+	UPROPERTY(EditAnywhere)
 	float Reach = 100.f;
-		
+
+	UPhysicsHandleComponent* PhysicsHandle = nullptr;
+	UInputComponent* InputComponent = nullptr;
+
+	void Grab();
+	void Release();
+	void FindPhysicsHandle();
+	void SetupInputComponent();
+
+	void DrawRaycast() const;
+
+	FHitResult GetFirstPhysicsBodyInReach() const;
+
 };
